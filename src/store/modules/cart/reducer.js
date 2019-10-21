e2019b8072d4ba3a8aeb42c004e673bdf16061c2
lilo <williamcco@gmail.com>
2019-10-21 18:03:31 -0300

    playing with immer

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,36 +1,30 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
-    case '@cart/ADD_SUCCESS': {
+    case '@cart/ADD_SUCCESS':
       return [...state, action.product];
-    }
-    case '@cart/REMOVE': {
-      const productIndex = state.findIndex(product => product.id === action.id);
+    case '@cart/REMOVE':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(
+          product => product.id === action.id
+        );
 
-      if (productIndex >= 0) {
-        const newState = [...state];
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
+    case '@cart/UPDATE_AMOUNT_SUCCESS':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(
+          product => product.id === action.id
+        );
 
-        newState.splice(productIndex, 1);
-        return newState;
-      }
-
-      return state;
-    }
-    case '@cart/UPDATE_AMOUNT_SUCCESS': {
-      if (action.amount <= 0) {
-        return state;
-      }
-
-      const productIndex = state.findIndex(product => product.id === action.id);
-
-      if (productIndex >= 0) {
-        const newState = [...state];
-
-        newState[productIndex].amount = action.amount;
-        return newState;
-      }
-
-      return state;
-    }
+        if (productIndex >= 0) {
+          // eslint-disable-next-line no-param-reassign
+          draft[productIndex].amount = action.amount;
+        }
+      });
     default:
       return state;
   }
